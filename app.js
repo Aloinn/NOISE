9f71cb5250fd0bbd983e279d891ccf4ce6b0af61
Aloinn <46081985+Aloinn@users.noreply.github.com>
2021-05-01 23:40:40 -0400

    Enabled CORS

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 8013;
+const cors = require('cors')
 
+app.use(cors())
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
