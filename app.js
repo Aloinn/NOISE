59be357591f63a543a7a797c29a496cb4140a526
Aloinn <46081985+Aloinn@users.noreply.github.com>
2021-05-01 20:12:19 -0400

    Fixed port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = 8013;
+const PORT = process.env.PORT || 8013;
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
